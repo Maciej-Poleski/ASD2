49005bc0f8aaf01179c89f2552792ad05e341e10
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2013-04-09 00:35:36 +0200

    K

--- K/satori.cxx
+++ K/satori.cxx
@@ -100,7 +100,7 @@ struct Node
     Node *MSM;
     bool terminal;
 
-    Node() : next({0,0}),MSM(0),terminal(false) {}
+    Node() : next( {0,0}),MSM(0),terminal(false) {}
 
     ~Node()
     {
@@ -135,17 +135,17 @@ static void doAC()
     root->MSM=NULL;
     std::queue<Pack> queue;
     if(root->next[0])
-        queue.push({root,0});
+        queue.push( {root,0});
     if(root->next[1])
-        queue.push({root,1});
+        queue.push( {root,1});
     while(!queue.empty())
     {
         Pack p=queue.front();
         queue.pop();
         if(p.getNode()->next[0])
-            queue.push({p.getNode(),0});
+            queue.push( {p.getNode(),0});
         if(p.getNode()->next[1])
-            queue.push({p.getNode(),1});
+            queue.push( {p.getNode(),1});
         if(p.parent==root)
         {
             p.getNode()->MSM=root;
@@ -161,28 +161,62 @@ static void doAC()
             else
                 p.getNode()->MSM=root;
 
-            if(p.getNode()->MSM->terminal)
-                p.getNode()->terminal=true;
+//             if(p.getNode()->MSM->terminal)
+//                 p.getNode()->terminal=true;
         }
     }
 }
+
+static Node *singlePattern[3000];
+
+static char text[3000][3000];
+static std::size_t c,d;
 
 static void compilePatterns()
 {
     delete root;
     root=new Node;
-    for(size_t i=0;i<a;++i)
+    for(size_t i=0; i<a; ++i)
     {
         Node *it=root;
-        for(size_t j=0;j<b;++j)
+        for(size_t j=0; j<b; ++j)
         {
             if(it->next[translateChar(pattern[i][j])]==NULL)
                 it->next[translateChar(pattern[i][j])]=new Node;
             it=it->next[translateChar(pattern[i][j])];
         }
         it->terminal=true;
+        singlePattern[i]=it;
     }
     doAC();
+}
+
+static Node* match[3000][3000];
+
+static void findMatch()
+{
+    for(size_t i=0; i<c; ++i)
+    {
+        // Tekstem jest wiersz i
+        Node *v=root;
+        for(size_t j=0; j<d; ++j)
+        {
+            char s=text[i][j];
+            if(s=='.')
+                v=NULL;
+            else
+                while(v && v->next[translateChar(s)]==NULL)
+                    v=v->MSM;
+
+            if(v==NULL)
+                v=root;
+            else
+                v=v->next[translateChar(s)];
+
+            if(v->terminal)
+                match[i][j]=v;
+        }
+    }
 }
 
 inline static void solution() __attribute__((optimize(3)));
@@ -195,13 +229,24 @@ inline static void solution()
     while(z--)
     {
         in>>a>>b;
-        for(std::size_t i=0;i<a;++i)
+        for(std::size_t i=0; i<a; ++i)
         {
-            in.ignore();
-            for(std::size_t j=0;j<b;++j)
+            in.ignore(std::numeric_limits< std::streamsize>::max(),'\n');
+            for(std::size_t j=0; j<b; ++j)
                 in>>pattern[i][j];
         }
         compilePatterns();
+        in>>c>>d;
+        for(std::size_t i=0; i<c; ++i)
+        {
+            in.ignore(std::numeric_limits< std::streamsize>::max(),'\n');
+            for(std::size_t j=0; j<d; ++j)
+            {
+                in>>text[i][j];
+                match[i][j]=NULL;
+            }
+        }
+        findMatch();
     }
 }
 

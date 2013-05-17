3b8bddada6d1c54901b8879fd88e3bf9279145ce
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2013-05-17 19:49:52 +0200

    T

--- T/satori.cxx
+++ T/satori.cxx
@@ -96,12 +96,12 @@ namespace
 {
 using namespace Wrapper;
 
-template<typename T>
-class LeftistTree : public std::tr1::enable_shared_from_this<LeftistTree<T> >
+template<typename T,class Compare = std::less<T> >
+class LeftistTree : public std::tr1::enable_shared_from_this<LeftistTree<T,Compare> >
 {
     struct Node
     {
-        friend class LeftistTree<T>;
+        friend class LeftistTree<T,Compare>;
 
     private:
         Node() : left(0), right(0),s(1) {}
@@ -150,47 +150,47 @@ public:
         v.root=t;
     }
 
-    std::tr1::shared_ptr<LeftistTree<T> > merge(std::tr1::shared_ptr<LeftistTree<T> > rhs);
+    std::tr1::shared_ptr<LeftistTree<T,Compare> > merge(std::tr1::shared_ptr<LeftistTree<T,Compare> > rhs);
     void deleteMin();
     void insert(const T &value);
 
 private:
     LeftistTree(Node *root) : root(root) {}
 
-    static std::tr1::shared_ptr<LeftistTree<T> >
-    merge(std::tr1::shared_ptr<LeftistTree<T> > lhs,
-          std::tr1::shared_ptr<LeftistTree<T> > rhs);
+    static std::tr1::shared_ptr<LeftistTree<T,Compare> >
+    merge(std::tr1::shared_ptr<LeftistTree<T,Compare> > lhs,
+          std::tr1::shared_ptr<LeftistTree<T,Compare> > rhs);
 
 private:
     Node *root;
 };
 
-template<typename T>
-std::tr1::shared_ptr<LeftistTree<T> >
-merge(std::tr1::shared_ptr<LeftistTree<T> > lhs,
-      std::tr1::shared_ptr<LeftistTree<T> > rhs)
+template<typename T,class Compare >
+std::tr1::shared_ptr<LeftistTree<T,Compare> >
+merge(std::tr1::shared_ptr<LeftistTree<T,Compare> > lhs,
+      std::tr1::shared_ptr<LeftistTree<T,Compare> > rhs)
 {
     return lhs.merge(rhs);
 }
 
-template<typename T>
-void swap(LeftistTree<T> &lhs,LeftistTree<T> &rhs)
+template<typename T,class Compare>
+void swap(LeftistTree<T,Compare> &lhs,LeftistTree<T,Compare> &rhs)
 {
     lhs.swap(rhs);
 }
 
-template<typename T>
-std::tr1::shared_ptr<LeftistTree<T> >
-LeftistTree<T>::merge(std::tr1::shared_ptr<LeftistTree<T> > rhs)
+template<typename T,class Compare >
+std::tr1::shared_ptr<LeftistTree<T,Compare> >
+LeftistTree<T,Compare>::merge(std::tr1::shared_ptr<LeftistTree<T,Compare> > rhs)
 {
     using std::swap;
-    std::tr1::shared_ptr<LeftistTree<T> > t = LeftistTree<T>::merge(this->shared_from_this(),rhs);
+    std::tr1::shared_ptr<LeftistTree<T,Compare> > t = LeftistTree<T,Compare>::merge(this->shared_from_this(),rhs);
     swap(*this,*t);
     return this->shared_from_this();
 }
 
-template<typename T>
-void LeftistTree<T>::deleteMin()
+template<typename T,class Compare>
+void LeftistTree<T,Compare>::deleteMin()
 {
     check(root);
     Node *left=root->left;
@@ -206,37 +206,37 @@ void LeftistTree<T>::deleteMin()
     else
     {
         using std::swap;
-        std::tr1::shared_ptr<LeftistTree<T> > l(new LeftistTree<T>(left));
-        std::tr1::shared_ptr<LeftistTree<T> > r(new LeftistTree<T>(right));
-        std::tr1::shared_ptr<LeftistTree<T> > t(merge(l,r));
+        std::tr1::shared_ptr<LeftistTree<T,Compare> > l(new LeftistTree<T,Compare>(left));
+        std::tr1::shared_ptr<LeftistTree<T,Compare> > r(new LeftistTree<T,Compare>(right));
+        std::tr1::shared_ptr<LeftistTree<T,Compare> > t(merge(l,r));
         swap(*this,*t);
     }
 }
 
-template<typename T>
-void LeftistTree<T>::insert(const T& value)
+template<typename T,class Compare >
+void LeftistTree<T,Compare>::insert(const T& value)
 {
     Node *node=new Node;
     node->value=value;
-    std::tr1::shared_ptr<LeftistTree<T> > t(new LeftistTree<T>(node));
+    std::tr1::shared_ptr<LeftistTree<T,Compare> > t(new LeftistTree<T,Compare>(node));
     merge(t);
 }
 
-template<typename T>
-std::tr1::shared_ptr<LeftistTree<T> >
-LeftistTree<T>::merge(std::tr1::shared_ptr<LeftistTree<T> > lhs,
-                      std::tr1::shared_ptr<LeftistTree<T> > rhs)
+template<typename T,class Compare >
+std::tr1::shared_ptr<LeftistTree<T,Compare> >
+LeftistTree<T,Compare>::merge(std::tr1::shared_ptr<LeftistTree<T,Compare> > lhs,
+                              std::tr1::shared_ptr<LeftistTree<T,Compare> > rhs)
 {
     if(lhs==rhs)
         return lhs;
-    std::tr1::shared_ptr<LeftistTree<T> > result(new LeftistTree<T>(Node::merge(lhs->root,rhs->root)));
+    std::tr1::shared_ptr<LeftistTree<T,Compare> > result(new LeftistTree<T,Compare>(Node::merge(lhs->root,rhs->root)));
     lhs->root=0;
     rhs->root=0;
     return result;
 }
 
-template<typename T>
-typename LeftistTree<T>::Node* LeftistTree<T>::Node::merge(Node* a, Node* b)
+template<typename T,class Compare>
+typename LeftistTree<T,Compare>::Node* LeftistTree<T,Compare>::Node::merge(Node* a, Node* b)
 {
     using std::swap;
     if(a==0)
@@ -246,13 +246,89 @@ typename LeftistTree<T>::Node* LeftistTree<T>::Node::merge(Node* a, Node* b)
 
     check(a);
     check(b);
-    if((a->value)>(b->value))
+    Compare comp;
+    if(comp(b->value,a->value))
         swap(a,b);
     a->right=merge(a->right,b);
     if((a->left==0) ||( (a->right!=0) && (a->left->s < a->right->s)))
         swap(a->left,a->right);
     a->s=(a->right?a->right->s:0)+1;
     return a;
+}
+
+struct Curve
+{
+    uint16_t left,right;
+    uint16_t id;
+    uint8_t color;
+    bool onStack;
+    std::vector<uint16_t> edges;
+
+    Curve() : color(255),onStack(false) {}
+};
+
+static Curve *G;
+static size_t n,k;
+
+const bool operator<(const Curve &lhs,const Curve &rhs) __attribute__((pure));
+const bool operator<(const Curve &lhs,const Curve &rhs)
+{
+    return (lhs.left<rhs.left) || ((lhs.left==rhs.left) && (lhs.right>rhs.right));
+}
+
+struct CurvePtrComp
+{
+    const bool operator()(const Curve * const &lhs,const Curve * const &rhs) const
+    {
+        return lhs->right<rhs->right;
+    }
+};
+
+static void makeColor(uint16_t k,uint8_t color)
+{
+    if(G[k].color!=255)
+        return;
+    G[k].color=color;
+    for(std::vector<uint16_t>::const_iterator i=G[k].edges.begin(),e=G[k].edges.end(); i!=e; ++i)
+    {
+        makeColor(*i,color^1);
+    }
+}
+
+struct CurveComp
+{
+    const bool operator()(const Curve &lhs,const Curve &rhs) const
+    {
+        return lhs.id<rhs.id;
+    }
+};
+
+struct Event
+{
+    uint16_t id;
+    bool right;
+};
+
+const bool operator==(const Event &lhs,const Event &rhs)
+{
+    return (lhs.id==rhs.id) && (lhs.right==rhs.right);
+}
+
+const bool operator<(const Event &lhs,const Event &rhs)
+{
+    if(lhs==rhs)
+        return false;
+    uint16_t l=lhs.right?G[lhs.id].right:G[lhs.id].left;
+    uint16_t r=rhs.right?G[rhs.id].right:G[rhs.id].left;
+    if(l!=r)
+        return l<r;
+    if(lhs.right!=rhs.right)
+        return lhs.right;
+    bool c=G[lhs.id]<G[rhs.id];
+    if(!lhs.right)
+        return c;
+    else
+        return !c;
 }
 
 inline static void solution()
@@ -262,58 +338,98 @@ inline static void solution()
     in >> z;
     //z=1;
     while (z--) {
-        size_t n,m;
-        in>>n>>m;
-        std::tr1::shared_ptr<LeftistTree< uint32_t> > *trees=new std::tr1::shared_ptr<LeftistTree<uint32_t> >[n];
-        for(size_t i=0;i<n;++i)
-            trees[i].reset(new LeftistTree<uint32_t>);
-        while(m--)
+        in>>n>>k;
+        G=new Curve[k];
+
+        for(size_t i=0; i<k; ++i)
         {
-            std::string command;
-            in>>command;
-            if(command[0]=='i')
+            uint16_t a,b;
+            in>>a>>b;
+            G[i].left=a;
+            G[i].right=b;
+            G[i].id=i;
+        }
+        std::sort(G,G+k);
+        typedef std::tr1::shared_ptr<LeftistTree<Curve*,CurvePtrComp> > TreePtr;
+        std::stack<TreePtr> stack;
+        for(size_t i=0; i<k; ++i)
+        {
+            while(!stack.empty())
             {
-                //insert
-                size_t k;
-                uint32_t e;
-                in>>k>>e;
-                trees[k]->insert(e);
-            }
-            else if(command[0]=='m')
-            {
-                // move
-                size_t s,t;
-                in>>s>>t;
-                trees[t]->merge(trees[s]);
-            }
-            else if(command[0]=='d')
-            {
-                // delmin
-                size_t k;
-                in>>k;
-                if(!trees[k]->isEmpty())
+                TreePtr tree=stack.top();
+                while(!tree->isEmpty())
                 {
-                    trees[k]->deleteMin();
+                    if(tree->getMin()->right<=G[i].left)
+                        tree->deleteMin();
+                    else
+                        break;
                 }
+                if(tree->isEmpty())
+                    stack.pop();
+                else
+                    break;
             }
-            else
+            TreePtr newConnectedComponent(new LeftistTree<Curve*,CurvePtrComp>());
+            newConnectedComponent->insert(&G[i]);
+            while(!stack.empty())
             {
-                check(command[0]=='g');
-                // getmin
-                size_t k;
-                in>>k;
-                if(!trees[k]->isEmpty())
+                TreePtr tree=stack.top();
+                if(tree->getMin()->right<G[i].right)
                 {
-                    out<<trees[k]->getMin()<<' ';
+                    G[i].edges.push_back(tree->getMin()-G);
+                    G[tree->getMin()-G].edges.push_back(i);
+                    newConnectedComponent->merge(tree);
+                    stack.pop();
+                }
+                else
+                    break;
+            }
+            stack.push(newConnectedComponent);
+        }
+        for(size_t i=0; i<k; ++i)
+            makeColor(i,0);
+        std::vector<Event> events[2];
+        for(size_t i=0; i<k; ++i)
+        {
+            check(G[i].color==0 || G[i].color==1);
+            events[G[i].color].push_back({i,false});
+            events[G[i].color].push_back({i,true});
+        }
+        std::sort(events[0].begin(),events[0].end());
+        std::sort(events[1].begin(),events[1].end());
+        bool ok=true;
+        for(size_t q=0; q<2 && ok; ++q)
+        {
+            std::stack<uint16_t> stack;
+            for(std::vector<Event>::const_iterator i=events[q].begin(),e=events[q].end(); i!=e && ok; ++i)
+            {
+                if(G[i->id].onStack)
+                {
+                    if(stack.top()==i->id)
+                    {
+                        stack.pop();
+                    }
+                    else
+                    {
+                        ok=false;
+                    }
                 }
                 else
                 {
-                    out<<"-1 ";
+                    stack.push(i->id);
+                    G[i->id].onStack=true;
                 }
             }
         }
-        out<<'\n';
-        delete [] trees;
+        std::sort(G,G+k,CurveComp());
+        if(ok)
+            for(size_t i=0; i<k; ++i)
+            {
+                out<<(G[i].color==0?"N\n":"S\n");
+            }
+        else
+            out<<"NIE\n";
+        delete [] G;
     }
 }
 
